231cee64c6a1a2d665131851278c7d5b44816076
Brian Du <brian.bh.du@gmail.com>
2017-04-11 15:28:00 -0400

    Added instane variable to all headers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,14 @@
 class PagesController < ApplicationController
 
   def welcome
-
+    @header = "This is the welcome page mang"
   end
 
   def about
+    @header = "I don't know what you heard about me"
   end
 
   def contest
-
+    @header = "All I do is win win WIN no matter WHAT"
   end
 end
