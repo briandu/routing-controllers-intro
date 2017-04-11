f9a5faab97f670afb63fcd48a3059225887e3db6
Brian Du <brian.bh.du@gmail.com>
2017-04-11 16:37:21 -0400

    Finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/welcome' => 'pages#welcome'
 
-  get '/' => 'pages#welcome'
+  root 'pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/about' => 'pages#about'
 
